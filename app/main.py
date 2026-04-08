05b726154c8259b280ecdcdcbc96ed661808bada
chtnnh <59027776+chtnnh@users.noreply.github.com>
2026-04-08 14:35:07 +0400

    feat: stage 3, attempt 1 (handling multiple concurrent clients)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 import socket
 
+"""
+How to implement this with an event loop?
+
+The event loop should be the main thread
+This thread will have the server that accepts connections
+"""
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,15 +15,16 @@ def main():
     pong = "+PONG\r\n".encode("utf-8")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.listen()
-    conn, return_address = server_socket.accept()
+    server_socket.listen(15)
 
-    with conn:
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendto(pong, return_address)
+    while True:
+        conn, return_address = server_socket.accept()
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendto(pong, return_address)
 
 
 if __name__ == "__main__":

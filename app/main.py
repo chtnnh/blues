063519186eea90129e567bf59b575e255a2a80d3
chtnnh <59027776+chtnnh@users.noreply.github.com>
2026-04-08 14:04:27 +0400

    feat: stage 2, attempt 1 (respond PONG to all incoming messages)

--- app/main.py
+++ app/main.py
@@ -5,8 +5,18 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
+    pong = r"+PONG\r\n".encode("utf-8")
+
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()  # wait for client
+    server_socket.listen()
+    conn, return_address = server_socket.accept()
+
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendto(pong, return_address)
 
 
 if __name__ == "__main__":

5baa9dc241945600d65701a5c111dc61e7cd20fb
chtnnh <59027776+chtnnh@users.noreply.github.com>
2026-04-08 14:07:42 +0400

    fix: stage 2, attempt 2 (string formatting issue)

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    pong = r"+PONG\r\n".encode("utf-8")
+    pong = "+PONG\r\n".encode("utf-8")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.listen()
